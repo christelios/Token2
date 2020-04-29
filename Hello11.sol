ad96bef146e9f6df39b96b70852a92ff192b54fb
Mznrahi786 <63340802+Mznrahi786@users.noreply.github.com>
2020-04-29 13:53:35 +0530

    Update Hello11.sol

--- Hello11.sol
+++ Hello11.sol
@@ -1,6 +1,3 @@
-Hello Sir,
-Please join me on Telegram or WhatsApp...
-Telegram: @Mzn_Rahi
-WhatsApp: +917417994131
-
-Please don't discuss on fiverr about Telegram or WhatsApp
+I cant share personal details. Order me on Fiverr
+Please don't discuss on fiverr about Telegram or WhatsApp.
+It may Block both of our account...
